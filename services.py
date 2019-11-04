46eb5a431929c594c3cef87e972950d97f429cf7
Sivabudh Umpudh <sivabudh.u@codium.co>
2019-11-04 23:45:21 +0700

    Got event_handler working too

--- services.py
+++ services.py
@@ -1,10 +1,12 @@
 from django_nameko_standalone import DjangoModels
+from nameko.events import EventDispatcher, event_handler
 from nameko.rpc import rpc
 
 
 class NamekoService:
     name = "nameko_service"
-    models = DjangoModels()  # Inject the django model dependency
+    models = DjangoModels()
+
 
     @rpc
     def search(self, search_text):
@@ -14,3 +16,12 @@ class NamekoService:
             return f"Question {search_text} is not found"
         else:
             return f"Question {question.question_text} is found. Date: {question.pub_date}"
+
+    @event_handler('ipms', 'import')
+    def imps_import(self, search_text):
+        from polls.models import Question
+        question = Question.objects.filter(question_text=search_text).first()
+        if not question:
+            print(f"Question {search_text} is not found")
+        else:
+            print(f"Question {question.question_text} is found. Date: {question.pub_date}")
